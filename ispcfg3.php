51233f6d70a9abd0617a245eba64de9db999c774
Shane Chrisp <shane@2000cn.com.au>
2018-05-30 09:48:02 +0800

    Removed the need to upload files to remote.d directory on ispconfig server for email quota.

--- ispcfg3.php
+++ ispcfg3.php
@@ -471,7 +471,12 @@ logModuleCall('ispconfig','URI',$soap_uri,$soap_url,'','');
         if ( $webcreation == 'on' ) {
             
             logModuleCall('ispconfig','PreCreateWebDomain',$website_id,$defaultwebserver,'','');
-            
+            $webphp = explode( ',', $tmpl['web_php_options'] );
+            if ( count( $webphp ) >= 0 ) {
+                $setphp = $webphp[0];
+            } else {
+                $setphp = null;
+            }
             $ispcparams = array(
                 'server_id'                 => $defaultwebserver,
                 'ip_address'                => '*',
@@ -481,6 +486,7 @@ logModuleCall('ispconfig','URI',$soap_uri,$soap_url,'','');
                 'vhost_type'                => 'name',
                 'hd_quota'                  => $tmpl['limit_web_quota'],
                 'traffic_quota'             => $tmpl['limit_traffic_quota'],
+                'php'                       => $setphp,
                 'cgi'                       => $tmpl['limit_cgi'],
                 'ssi'                       => $tmpl['limit_ssi'],
                 'perl'                      => $tmpl['limit_perl'],
@@ -552,7 +558,7 @@ logModuleCall('ispconfig','URI',$soap_uri,$soap_url,'','');
                     'parent_domain_id'  => $website_id,
                     'username'          => $username . $ftpsuffix,
                     'password'          => $password,
-                    'quota_size'        => $tmpl['limit_web_domain'],
+                    'quota_size'        => $tmpl['limit_web_quota'],
                     'active'            => 'y',
                     'uid'               => $domain_arr['system_user'],
                     'gid'               => $domain_arr['system_group'],
@@ -1337,6 +1343,8 @@ function ispcfg3_ClientArea( $params ) {
     }
     $domain_url = ($params['serversecure'] == 'on' ? 'https://' : 'http://').$params['domain'];
 
+    print_r($vars['_lang']);
+    
     $requestedView = isset($_REQUEST['view']) ? $_REQUEST['view'] : '';
     if ($requestedView && $requestedView != 'overview') {
         $viewResponse = cwispy_handle_view($requestedView, $params);
