f474544e8a27ea49788a1c7c3116e0eb6d3abc18
MasteRTriX <mastertrix@doxma.com>
2020-05-27 21:15:29 -0300

    Create spanish.php

---
+++ lang/spanish.php
@@ -0,0 +1,42 @@
+<?php
+
+/** Copy the below to your lang/overrides/english.php */
+
+$_LANG['ispcfg3_add_cron_job'] = "Agregar tarea programada";
+$_LANG['ispcfg3_add_dns_record'] = "Agregar registro DNS";
+$_LANG['ispcfg3_alias_domains'] = "Alias de dominio";
+$_LANG['ispcfg3_cron_command'] = "Comando";
+$_LANG['ispcfg3_cron_jobs'] = "Tareas programadas";
+$_LANG['ispcfg3_current_cron_jobs'] = "Tareas programadas actuales";
+$_LANG['ispcfg3_current_dns_records'] = "Registros DNS actuales";
+$_LANG['ispcfg3_databases'] = "Bases de datos";
+$_LANG['ispcfg3_day'] = "Día";
+$_LANG['ispcfg3_dns_description'] = "DNS holds records such as the address of the server that handles e-mail, the web server, mail server among others. The default DNS settings are already configured for you.";
+$_LANG['ispcfg3_dns_records'] = "Registros DNS";
+$_LANG['ispcfg3_delete_dns_record'] = "Borrar registro DNS";
+$_LANG['ispcfg3_delete_dns_confirm'] = "Está seguro de borrar este registro DNS?";
+$_LANG['ispcfg3_emails'] = "Correos";
+$_LANG['ispcfg3_email_forwarders'] = "Reenviadores de correo";
+$_LANG['ispcfg3_file_manager'] = "Administrador de archivos";
+$_LANG['ispcfg3_ftp_accounts'] = "Cuentas FTP";
+$_LANG['ispcfg3_hour'] = "Hora";
+$_LANG['ispcfg3_host'] = "Servidor";
+$_LANG['ispcfg3_manage_cron_jobs'] = "Administrar tareas programadas";
+$_LANG['ispcfg3_manage_dns_records'] = "Administrar registros DNS";
+$_LANG['ispcfg3_minute'] = "Minuto";
+$_LANG['ispcfg3_month'] = "Mes";
+$_LANG['ispcfg3_no_dns_records_found'] = "No se encontraron registros DNS";
+$_LANG['ispcfg3_of'] = "de";
+$_LANG['ispcfg3_points_to'] = "Apunta a";
+$_LANG['ispcfg3_sub_domains'] = "Sub dominios";
+$_LANG['ispcfg3_ttl'] = "TTL";
+$_LANG['ispcfg3_type'] = "Tipo";
+$_LANG['ispcfg3_update_dns_record'] =  "Actualizar registro DNS";
+$_LANG['ispcfg3_unlimited'] = "Ilimitado";
+$_LANG['ispcfg3_usage_stats'] = "Estadísticas de uso";
+$_LANG['ispcfg3_websites'] = "Sitios web";
+$_LANG['ispcfg3_weekday'] = "Día de la semana";
+$_LANG['ispcfg3_product_details'] = "Detalles del producto";
+$_LANG['ispcfg3_product_tools'] = "Herramientas del producto";
+$_LANG['ispcfg3_login_to'] = "Ingresar en";
+$_LANG['ispcfg3_website_statistics'] = "Estadísticas del sitio web";
